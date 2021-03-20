3316eed5b8b4c21a2a8114702454685c0e9db416
Harsh Singhal <harshsinghal932941@gmail.com>
2021-03-20 23:08:47 +0530

    heroku

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express=require("express");
 const app=express();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 8080;
 const hbs=require("hbs");
 require("./db/conn");
 require("dotenv").config();
-app.get("",(req,res)=>{
 
-});
 app.use(express.json());
+
 app.set('view engine','hbs');
+app.get("",(req,res)=>{
+    res.send("hiii i am here");
+});
 app.use("/api/files",require("./route/files"))
 app.use("api/files/send",require("./route/files"));
 app.use("/files",require("./route/show"));
 app.use("/files/download",require("./route/download"));
+
 app.listen(port,()=>{
     console.log(port);
 });
